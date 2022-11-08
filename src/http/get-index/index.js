d9f296fee96a47ace039f39000a940d706dfdca3
kezh <kezh@ffcs.cn>
2022-11-08 15:57:00 +0800

    test: 测试静态资源

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -9,6 +9,6 @@ exports.handler = async function Index () {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body: arc.static('index.html')
+    body: arc.static('background.jpg')
   }
 }
